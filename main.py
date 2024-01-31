d88210409338b94fe51a61ce89221131aad06702
Damien Foulon <foulon.damien.pro@gmail.com>
2024-01-31 02:19:45 +0100

    Show date and time on app

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.clock import Clock
 from weather import getWeather
+from date import getDate, getTime
 
 class MirrOS(App):
     def build(self):
-        return Label(text=f"{str(getWeather()['current_temperature'])}°C")
+        # Create a label widget
+        self.label = Label()
 
+        # Schedule the update function to be called every minute
+        Clock.schedule_interval(self.update, 1  # 60 seconds = 1 minute
+
+        return self.label
+
+    def update(self, *args):
+        # Update the label text with the current date and time
+        self.label.text = f"Nous sommes le {getDate()} et il est {getTime()}"
 
 if __name__ == '__main__':
     MirrOS().run()
 
-# Path: main.kv
+# Path: main.py
