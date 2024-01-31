d2b80cbfa4bc6444d47de100078760354f85d43b
Damien Foulon <foulon.damien.pro@gmail.com>
2024-01-31 02:08:36 +0100

    Remove time.py and add features into date.py

--- date.py
+++ date.py
@@ -1,6 +1,7 @@
 import os
-from datetime import date
+import pytz
 import locale
+from datetime import date, datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,6 +9,7 @@ load_dotenv()
 # Import environment variables
 LANGUAGE = os.getenv("LANGUAGE")
 DATE_FORMAT = os.getenv("DATE_FORMAT")
+COUNTRY_CODE = os.environ.get("COUNTRY_CODE")
 
 today = date.today()
 
@@ -24,6 +26,30 @@ def setLang(lang):
         case _:
             locale.setlocale(locale.LC_TIME, switcher.get(lang))
             print('Set language to : ', switcher.get(lang, "Invalid language"))
+
+        # Get UTC from country code
+def get_utc(country_code):
+    try:
+        # Load the timezone for the given country code
+        country_timezone = pytz.country_timezones[country_code][0]
+
+        # Get the timezone object
+        timezone_object = pytz.timezone(country_timezone)
+
+        # Get the current time
+        current_time = datetime.now()
+
+        # Get the UTC offset for the current time
+        utc_offset = timezone_object.utcoffset(current_time).total_seconds() / 3600
+
+        return {"utc": utc_offset, "timezone": timezone_object, "country_timezone": country_timezone}
+    except KeyError:
+        return None
+
+
+def getTime():
+    current_time = datetime.now(get_utc(COUNTRY_CODE)["timezone"])
+    return current_time.strftime("%H:%M")
 
 
 def getDate():
